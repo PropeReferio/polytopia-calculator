d9ee7ba40341108a2bd1d2e9ece808ec10615066
Arjun Pravin <arjunpravin@Arjuns-MacBook-Air.local>
2021-05-04 19:33:11 -0400

    Added a attack method

--- main.js
+++ main.js
@@ -5,4 +5,12 @@ class PolyUnit {
 		this.max_hp = max_hp;
 		this.cur_hp = cur_hp;
   }
+
+   attack(defender) {
+     attackForce = this.attack * (this.cur_hp/this.max_hp);
+     defenseForce = defender.defense * (defender.cur_hp/defender.max_hp);
+     totalDamage = attackForce + defenseForce;
+     this.cur_hp -= round((attackForce/totalDamage)*this.attack*4.5);
+     defender.cur_hp -= round((defenseForce/totalDamage)*defender.defense*4.5);
+   }
 }
