9706cf3c29106f05aded731e3843f434af264883
Jeanine Beckle <jeaninebeckle@gmail.com>
2021-05-04 19:35:23 -0500

    wip

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ class PolyUnit {
 		this.cur_hp = cur_hp;
   }
 }
+
+
+const changeAttacker = (polyUnitObj) => {
+  $('#attackerCurrentHP').val() = polyUnitObj.cur_hp
+  $('#attackerMaxHP').val() = polyUniObj.max_hp
+}
+
+const changeDefender = (polyUnitObj) => {
+  $('#defenderCurrentHP').val() = polyUnitObj.cur_hp
+  $('#defenderMaxHP').val() = polyUniObj.max_hp
+}
