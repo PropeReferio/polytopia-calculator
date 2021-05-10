69605399c3bd798e8572ec0403c2118dd916c331
PropeReferio <lemuel.b.stevens@gmail.com>
2021-05-10 18:26:12 -0500

    added fields and started damage calculator function

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const units = {
+	"Warrior": new PolyUnit(2, 2, 10, 10),
+	"Defender": new PolyUnit(1, 3, 15, 15)
+}
+
 class PolyUnit {
   constructor(attack, defense, max_hp, cur_hp) {
     this.attack = attack;
@@ -5,7 +10,6 @@ class PolyUnit {
 		this.max_hp = max_hp;
 		this.cur_hp = cur_hp;
   }
-
 
    attack(defender) {
      attackForce = this.attack * (this.cur_hp/this.max_hp);
@@ -24,4 +28,14 @@ const changeAttacker = (polyUnitObj) => {
 const changeDefender = (polyUnitObj) => {
   $('#defenderCurrentHP').val() = polyUnitObj.cur_hp
   $('#defenderMaxHP').val() = polyUniObj.max_hp
+}
+
+const calculateDamage = () => {
+	const attackerAttack = document.getElementById('attackerAttack').value;
+	const attackerCurHP = document.getElementById('attackerCurHP').value;
+	const attackerMaxHP = document.getElementById('attackerMaxHP').value;
+
+	const defenderDefense = document.getElementById('defenderDefense').value;
+	const defenderCurHP = document.getElementById('defenderCurHP').value;
+	const defenderMaxHP = document.getElementById('defenderMaxHP').value;
 }
